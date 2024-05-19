88dc97c60455fffaf1c5bd33213c8d5580183603
Emmanuel de Jesús Guzmán Benítez <finnemm@gmail.com>
2024-05-19 17:21:07 -0600

    :D

---
+++ src/main/java/com/example/whatsapp/HelloApplication.java
@@ -0,0 +1,25 @@
+package com.example.whatsapp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        Scene scene = new Scene(root);
+
+        primaryStage.setTitle("Wazap B)");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
